c8f5b06d1df9956d662e0009acbd3f153d6f0ef0
John Vincent Racasa <65653074+johnvincentracasa@users.noreply.github.com>
2021-04-15 20:41:59 +0800

    Add files via upload

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+const header = document.querySelector('.header');
+const mobileMenuBtn = document.querySelector('.header__menu');
+const overlayLinks = document.querySelector('.overlay');
+
+mobileMenuBtn.onclick = function () {
+	header.classList.toggle('open');
+};
